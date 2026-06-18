d648bddc008defdd66afe691648a54eb2d9f1017
Oleh Mytsyk <oleh.mytsyk.25@kse.org.ua>
2026-06-18 15:22:42 +0300

    Добавив CTE яка робить UNION ALL та повертає певну інформацію.

--- assignment.sql
+++ assignment.sql
@@ -69,27 +69,46 @@ values
 (2, 1, '2026-03-11', 'Cancelled'),
 (3, 2, '2026-03-10', 'Completed'),
 (4, 3, '2026-03-12', 'In-progress'),
-(5, 4, '2026-03-15', 'Cancelled');
+(5, 4, '2026-03-15', 'Cancelled'),
+(3, 1, '2026-01-11', 'Completed'),
+(2, 4, '2026-01-11', 'Completed');
 
-select 
-  depart.departmentname,
-  count(app.appointmentid) as TotalCompletedAppointments,
-  sum(bill.amount) as TotalRevenue
-from 
-  departaments depart
-join 
-  doctors docs on depart.departmentid = docs.departmentid
-join 
-  appointment app on docs.doctorid = app.doctorid
-join 
-  patients pats on app.patientid  = pats.patientid
-join
-  bills bill on pats.patientid = bill.patientid
-where
-  pats.age > 18
-  and app.status = 'Completed'
-group by
-  depart.departmentname,
-  depart.departmentid
-order by
-  totalrevenue desc;
+insert into
+Bills(PatientID, Amount, Status)
+values
+(1, 150.00, 'Paid'),
+(2, 50.00, 'Paid'),
+(3, 200.00, 'Paid'),
+(4, 300.00, 'Unpaid'),
+(5, 150.00, 'Paid');
+
+with CTE as (
+	select 
+		'Adults' as PatientCategory,
+		dep.departmentname as DepartmentName,
+		count(distinct apps.appointmentid) as TotalApps,
+		coalesce(sum(bill.amount), 0) as TotalRevenue
+	from departaments dep
+	join doctors docs on docs.departmentid = dep.departmentid
+	join appointment apps on docs.doctorid = apps.doctorid
+	join patients pats on apps.patientid = pats.patientid
+	join bills bill on bill.patientid = pats.patientid
+	where pats.age > 20
+	group by dep.departmentname
+	
+	union all
+	
+	select 
+		'Youngsters' as PatientCategory,
+		dep.departmentname as DepartmentName,
+		count(distinct apps.appointmentid) as TotalApps,
+		coalesce(sum(bill.amount), 0) as TotalRevenue
+	from departaments dep
+	join doctors docs on docs.departmentid = dep.departmentid
+	join appointment apps on docs.doctorid = apps.doctorid
+	join patients pats on apps.patientid = pats.patientid
+	join bills bill on bill.patientid = pats.patientid
+	where pats.age <= 20
+	group by dep.departmentname
+)
+select * from CTE;
